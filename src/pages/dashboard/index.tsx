dcbbbfdee8f22cf18cbf7a390987c877da3d743f
lypeolivers <105740860+lypeolivers@users.noreply.github.com>
2022-10-28 10:38:32 -0300

    feat: testando se vem conflito

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -71,11 +71,13 @@ function Dashboard() {
               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
               reprehenderit in voluptate velit esse cillum dolore eu fugiat
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
+              sunt in culpa qui officia deserunt mollit anim id est laborum." aaohdsaisudad
             </p>
 
             {/* INSERIR DIV OU TAGS PRA GERAR CONFLITO */}
-
+              <div>
+                <p>a</p>
+              </div>
           </ConteinerItens>
         </>
       )}
